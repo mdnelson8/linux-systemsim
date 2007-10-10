6e289b42008d20546b9ed2e9b8b9f8d00bef1978
Eric Van Hensbergen <ericvh@strongmad.(none)>
2007-10-10 10:47:41 -0500

    IBM Full System Simulator (systemsim) patch
    Signed-off-by: Eric Van Hensbergen <bergevan@us.ibm.com>

--- arch/powerpc/kernel/udbg.c
+++ arch/powerpc/kernel/udbg.c
@@ -42,6 +42,9 @@ void __init udbg_early_init(void)
 #elif defined(CONFIG_PPC_EARLY_DEBUG_MAPLE)
 	/* Maple real mode debug */
 	udbg_init_maple_realmode();
+#elif defined(CONFIG_PPC_EARLY_DEBUG_FSS)
+	/* Maple real mode debug */
+	udbg_init_fss();
 #elif defined(CONFIG_PPC_EARLY_DEBUG_ISERIES)
 	/* For iSeries - hit Ctrl-x Ctrl-x to see the output */
 	udbg_init_iseries();
